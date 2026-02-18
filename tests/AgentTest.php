4ba3e5710b5c4e1d361c1e7e0bbdec3f2f058bce
Jennifer Brehm <jennifer.brehm@ilscipio.com>
2026-02-18 09:32:31 +0100

    Add PHPUnit config and placeholder test

---
+++ tests/AgentTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AIVory\Monitor\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AgentTest extends TestCase
+{
+    public function testModuleLoads(): void
+    {
+        $this->assertTrue(true);
+    }
+}
